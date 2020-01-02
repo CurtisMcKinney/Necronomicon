f18f1791588bd47131dfa5951c9086adb3ea3fef
Chad McKinney <chad@chadmckinneyaudio.com>
2020-01-01 18:08:23 -0800

    Fixed unused warnings

--- source/mach/mach_ast.c
+++ source/mach/mach_ast.c
@@ -1478,11 +1478,13 @@ void necro_mach_program_init_base_and_runtime(NecroMachProgram* program)
         ast_symbol->core_ast_symbol->is_primitive = true;
         NecroMachAstSymbol* mach_symbol           = necro_mach_ast_symbol_create_from_core_ast_symbol(&program->arena, ast_symbol->core_ast_symbol);
         mach_symbol->is_primitive                 = true;
+#if 0 // unused
         NecroMachType*      fn_type               = necro_mach_type_create_fn(&program->arena, program->type_cache.word_uint_type, (NecroMachType*[]) { program->type_cache.word_int_type, program->type_cache.word_uint_type }, 2);
         if (program->word_size == NECRO_WORD_4_BYTES)
             necro_mach_create_runtime_fn(program, mach_symbol, fn_type, (NecroMachFnPtr)necro_runtime_print_i32, NECRO_STATE_POINTWISE)->fn_def.symbol;
         else
             necro_mach_create_runtime_fn(program, mach_symbol, fn_type, (NecroMachFnPtr)necro_runtime_print_i64, NECRO_STATE_POINTWISE)->fn_def.symbol;
+#endif
     }
 
     // necro_runtime_print_i64
@@ -1503,11 +1505,13 @@ void necro_mach_program_init_base_and_runtime(NecroMachProgram* program)
         ast_symbol->core_ast_symbol->is_primitive = true;
         NecroMachAstSymbol* mach_symbol           = necro_mach_ast_symbol_create_from_core_ast_symbol(&program->arena, ast_symbol->core_ast_symbol);
         mach_symbol->is_primitive                 = true;
+#if 0 // unused
         NecroMachType*      fn_type               = necro_mach_type_create_fn(&program->arena, program->type_cache.word_uint_type, (NecroMachType*[]) { program->type_cache.word_uint_type, program->type_cache.word_uint_type }, 2);
         if (program->word_size == NECRO_WORD_4_BYTES)
             necro_mach_create_runtime_fn(program, mach_symbol, fn_type, (NecroMachFnPtr)necro_runtime_print_u32, NECRO_STATE_POINTWISE)->fn_def.symbol;
         else
             necro_mach_create_runtime_fn(program, mach_symbol, fn_type, (NecroMachFnPtr)necro_runtime_print_u64, NECRO_STATE_POINTWISE)->fn_def.symbol;
+#endif
     }
 
     // necro_runtime_print_float
